e16814b491aa700c800b980e961a25912804cc2c
Aleks4579 <42387392+Aleks4579@users.noreply.github.com>
2022-10-26 22:17:26 +0300

    Delete module-info.java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.nasledovanie {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.nasledovanie to javafx.fxml;
-    exports com.example.nasledovanie;
-}
